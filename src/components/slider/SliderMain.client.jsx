d0b603d29f3945af1de667d15521f0c823c829fa
maria fernanda monoga mejia <mariafernandamonogamejia@gmail.com>
2022-11-02 13:34:58 -0500

    solucion de error libreria swiper

--- src/components/slider/SliderMain.client.jsx
+++ src/components/slider/SliderMain.client.jsx
@@ -1,9 +1,6 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 export function SliderMain({arrayDesk, arrayMobile}) {
     let listItems;
